9c55f603b433d7671a416492e54863982e21ed4f
TMXB <107414284+tmxb1@users.noreply.github.com>
2026-07-06 18:21:53 +0800

    Update test_web.py

--- test_web.py
+++ test_web.py
@@ -95,8 +95,6 @@ class App:
             stealth_sync(self.page)
 
             self.page.goto("https://www.wenku8.net/login.php")
-            self.page.fill("input[name='username']", username)
-            self.page.fill("input[name='password']", password)
             self.page.click("input[name='submit']")
             self.page.wait_for_timeout(2000)
 
